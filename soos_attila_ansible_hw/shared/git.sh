b98dfa070e6f7384bdcc3d5432ee7f88ffaecea4
soosati <soosatti@gmail.com>
2025-02-01 15:25:54 +0100

    gitignore delete

--- soos_attila_ansible_hw/shared/git.sh
+++ soos_attila_ansible_hw/shared/git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-# a feladat kiírás szerint a publikus repository-ból letölti a tartalma az aktuális lokális helyre
+# a feladat kiírás szerint a publikus repository-ból letölti a tartalmat az aktuális lokális helyre
