1a994a158fe1892dc6f0afc41fa0b0763272cb67
soosati <soosatti@gmail.com>
2025-02-01 16:32:07 +0100

    git.sh feltöltése - végre jönnek a fájlok

--- soos_attila_ansible_hw/shared/git.sh
+++ soos_attila_ansible_hw/shared/git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # a feladat kiírás szerint a publikus repository-ból letölti a tartalmat az aktuális lokális helyre
-https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/nginx_install.yml?token=GHSAT0AAAAAAC5LJTEGBMTUCR5YASTBBS5QZ46GIXA
-https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/inventory?token=GHSAT0AAAAAAC5LJTEHSQN5ANIZ7BYTUAUIZ46GJIQ
-https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/my_index.j2?token=GHSAT0AAAAAAC5LJTEGMJOMHHJNLI5X3UTOZ46GJPQ
+curl -OL "https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/inventory"
+curl -OL "https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/my_index.j2"
+curl -OL "https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/nginx_install.yml"
