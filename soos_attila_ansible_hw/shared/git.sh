554ac1ff0c1f2d4a90c26cd8b2d6809d85ea8476
soosati <soosatti@gmail.com>
2025-02-01 15:37:55 +0100

    repository feltöltése

--- soos_attila_ansible_hw/shared/git.sh
+++ soos_attila_ansible_hw/shared/git.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # a feladat kiírás szerint a publikus repository-ból letölti a tartalmat az aktuális lokális helyre
+curl -LJO https://raw.githubusercontent.com/soosatti/Soos_Attila_ansible_hw/refs/heads/master/soos_attila_ansible_hw/shared/nginx_install.yml?token=GHSAT0AAAAAAC5LJTEHUAHWF3AGVN3IWFW2Z46GFHA
